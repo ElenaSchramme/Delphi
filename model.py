ca563259994f10d55ad780866bc5d6c51368e4e2
ElenaSchramme <105346841+ElenaSchramme@users.noreply.github.com>
2024-09-23 15:08:11 +0200

    deacktivate kvcache for comparison

--- model.py
+++ model.py
@@ -167,7 +167,7 @@ class DelphiConfig:
     bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
     mask_ties: bool = False
     ignore_tokens: list = field(default_factory=lambda: [0])
-    use_kvcache: bool = True
+    use_kvcache: bool = False
 
 class Delphi(nn.Module):
 
